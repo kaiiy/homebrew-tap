bec26c7c365c0160d66d22300be296855c5dfb09
kaiiy <81578246+kaiiy@users.noreply.github.com>
2024-01-13 16:10:28 +0000

    Update Mohaya to v0.3.0

--- Formula/mohaya.rb
+++ Formula/mohaya.rb
@@ -4,8 +4,8 @@
 class Mohaya < Formula
   desc ""
   homepage "https://github.com/kaiiy/mohaya/"
-  url "https://github.com/kaiiy/mohaya/archive/refs/tags/v0.2.3.tar.gz"
-  sha256 "8802b25c9a53014d8d772ecec7b4f3f5ee6972e2a3b61ff042c5ead7f9c334b9"
+  url "https://github.com/kaiiy/mohaya/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "44e28c2081a22a45880e3626e95128321a52bc4dedb4e8896d24f5f87da72d12"
   license "MIT"
 
   depends_on "deno"
