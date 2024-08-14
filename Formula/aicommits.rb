c490addbb7c7bfd636b3e7337139b55d6ef4c846
kaiiy <kaiiy@users.noreply.github.com>
2024-08-14 15:04:55 +0000

    Apply automatic changes

--- Formula/aicommits.rb
+++ Formula/aicommits.rb
@@ -1,8 +1,8 @@
 class Aicommits < Formula
     desc ""
     homepage "https://github.com/kaiiy/aicommits/"
-    url "https://github.com/kaiiy/aicommits/archive/refs/tags/v2.2.1.tar.gz"
-    sha256 "32c4792ac47e7e836bb472b57d72242a8dcd880256c2bd707b5cf3afb5d35f8a"
+    url "https://github.com/kaiiy/aicommits/archive/refs/tags/v2.3.0.tar.gz"
+    sha256 "4c62a673726ea8d2abb5577c38870b149663db71c9b6517efe219009af73002d"
     license "MIT"
   
     depends_on "deno"
