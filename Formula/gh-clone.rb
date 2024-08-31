a03482e25bf5d61faa2046b0d528ea1b686937d2
kaiiy <kaiiy@users.noreply.github.com>
2024-08-31 14:10:39 +0000

    Apply automatic changes

--- Formula/gh-clone.rb
+++ Formula/gh-clone.rb
@@ -1,8 +1,8 @@
 class GhClone < Formula
     desc ""
     homepage "https://github.com/kaiiy/gh-clone/"
-    url "https://github.com/kaiiy/gh-clone/archive/refs/tags/v0.1.0_1.tar.gz"
-    sha256 "2fc8866f679000fa54a2bd54d1a9992568fd061385dd3e1c7ee11d2db8ea409f"
+    url "https://github.com/kaiiy/gh-clone/archive/refs/tags/v0.1.1.tar.gz"
+    sha256 "b605ea7899f08afa0553f36fd623ce434f5f6cdf4f618076d2771ded03dfe9c4"
     license "MIT"
   
     depends_on "deno"
