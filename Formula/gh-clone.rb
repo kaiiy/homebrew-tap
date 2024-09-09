46968b5e2bc374c0cf5b800d1ac3ae9168cfce79
kaiiy <81578246+kaiiy@users.noreply.github.com>
2024-09-09 13:09:22 +0900

    Update gh-clone.rb

--- Formula/gh-clone.rb
+++ Formula/gh-clone.rb
@@ -4,6 +4,8 @@ class GhClone < Formula
     url "https://github.com/kaiiy/gh-clone/archive/refs/tags/v0.1.3.tar.gz"
     sha256 "770176b079adc3adc0f2f50d4b6ae494b3d8c00ac82b872e46e6373656063e9a"
     license "MIT"
+
+    depends_on "deno"
   
     def install
       bin.install "bin/gh-clone"
