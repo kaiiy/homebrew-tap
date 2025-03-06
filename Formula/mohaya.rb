774a43c672376896e6b960f7ea3596642975f81a
kaiiy <81578246+kaiiy@users.noreply.github.com>
2025-03-06 06:47:13 +0000

    Apply automatic changes

--- Formula/mohaya.rb
+++ Formula/mohaya.rb
@@ -4,8 +4,8 @@
 class Mohaya < Formula
   desc ""
   homepage "https://github.com/kaiiy/mohaya/"
-  url "https://github.com/kaiiy/mohaya/archive/refs/tags/v1.6.0.tar.gz"
-  sha256 "f7f4d9e47a1759f2e89d92efab8a805f3887b57890593d6971cf4366683a261e"
+  url "https://github.com/kaiiy/mohaya/archive/refs/tags/v1.7.0.tar.gz"
+  sha256 "4bb93cfaa41df58553f557cae85ab1d05300645bfbb8a0a56ddfcd5f2955e824"
   license "MIT"
 
   depends_on "deno"
